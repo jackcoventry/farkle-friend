bef5da254c489e0e0689e6a37a23c22f5bd93394
Jack Coventry <jackcoventry@gmail.com>
2025-12-04 19:46:47 +0000

    Function for banking calculating banked dice and updating temporary score

--- src/domain/game/turnLogic.ts
+++ src/domain/game/turnLogic.ts
@@ -1,4 +1,4 @@
-import { DieValue, getScoringInfo, rollDice } from "./dice";
+import { DieValue, getScoringInfo, rollDice, scoreSelectedDice } from "./dice";
 
 export type ActiveTurn = {
   availableDice: number;
@@ -41,6 +41,33 @@ export function rollInActiveTurn(turn: ActiveTurn): ActiveTurn {
     ...turn,
     currentRoll: dice,
     isFarkled: false,
+  };
+}
+
+export function bankDiceFromCurrentRoll(
+  heldIndices: number[],
+  turn: ActiveTurn
+): ActiveTurn {
+  if (!turn.currentRoll || turn.isComplete || heldIndices?.length === 0) {
+    return turn;
+  }
+
+  const heldDice: DieValue[] = heldIndices
+    .map((index) => turn.currentRoll![index])
+    .filter((value): value is DieValue => value !== null);
+
+  const score = scoreSelectedDice(heldDice);
+  if (score <= 0) return turn;
+
+  const remainingDice = turn.currentRoll.length - heldDice.length;
+  // Gets next available dice count
+  const nextAvailable = remainingDice === 0 ? 6 : remainingDice;
+
+  return {
+    ...turn,
+    availableDice: nextAvailable,
+    currentRoll: null,
+    tempScore: turn.tempScore + score,
   };
 }
 
