31ac1dd31617dee286c7e6a764d28b818f985758
Jack Coventry <jackcoventry@gmail.com>
2026-04-28 14:22:21 +0100

    Fixed some test issues

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,22 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(dirname, "src"),
+    },
+  },
   test: {
     environment: "jsdom",
+    fileParallelism: false,
+    globals: true,
+    maxWorkers: 1,
+    pool: "threads",
     setupFiles: ["./config/setupTests.ts"],
   },
 });
