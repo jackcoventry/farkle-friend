e3e9d3aaf4f24f9e6cdefa41a58c2e7aa2a0f55d
Jack Coventry <jackcoventry@gmail.com>
2026-05-26 13:43:37 +0100

    Added custom breakpoint support with post-css

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,14 @@
+import postcssGlobalData from '@csstools/postcss-global-data';
+import customMedia from 'postcss-custom-media';
+
 const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
+  plugins: [
+    postcssGlobalData({
+      files: ['./src/styles/breakpoints.css'],
+    }),
+    customMedia(),
+    '@tailwindcss/postcss',
+  ],
 };
 
 export default config;
